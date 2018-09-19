56a4bfffa70130ab8bb59215180ecb983d4e9db0
tastafur <ruben.romero@guidesmiths.com>
2018-09-20 00:02:51 +0200

    add bodyparser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const rp = require('request-promise');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
