a2c3bd9a6d6a426be579fcd662ae538b2a8f27f7
tastafur <ruben.romero@guidesmiths.com>
2018-09-20 00:18:03 +0200

    add port

--- index.js
+++ index.js
@@ -38,4 +38,4 @@ app.get('/phones', (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
